2f7e9100c4f1e47cc8159332e38abea922713ad0
PadalaSandeep <sandeep.dm@live.com>
2022-04-09 14:25:42 +0530

    Added cypress-mochawesome-reporter

---
+++ cypress/integration/my-first-api-test.js
@@ -0,0 +1,11 @@
+/// <reference types="cypress" />
+
+it('Get users', function(){
+   cy.request({
+       method : 'GET',
+       url : 'https://reqres.in/api/users/3'
+   }).then((response)=>{
+       console.log(response)
+       expect(response.status).to.eq(200)
+   })
+})
