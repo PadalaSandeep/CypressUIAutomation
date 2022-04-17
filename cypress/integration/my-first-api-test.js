c53b0e1c59f07fdc6fab94968ae97f5489b92869
PadalaSandeep <sandeep.dm@live.com>
2022-04-17 23:03:38 +0530

    Adding Post Test

--- cypress/integration/my-first-api-test.js
+++ cypress/integration/my-first-api-test.js
@@ -1,5 +1,6 @@
 /// <reference types="cypress" />
 
+
 it('Get users', function(){
    cy.request({
        method : 'GET',
@@ -10,4 +11,20 @@ it('Get users', function(){
        expect(response.status).to.eq(200)
        expect(response.body.data).has.property('id', 3)
    })
+})
+
+it('Post test', function(){
+  cy.request({
+    method : 'POST',
+    url : 'https://reqres.in/api/users',
+    body: {
+        "name": "morpheus",
+        "job": "leader"
+    }
+  }) .should((response)=>{
+    console.log(response)
+    expect(response.status).to.eq(200)
+    expect(response.body.data).has.property('id', 3)
+})
+    
 })
