b48f9fb3086220f4e7f8e3b30aae7169bca7c44c
PadalaSandeep <sandeep.dm@live.com>
2022-04-17 22:21:26 +0530

    Update Allure Report

--- cypress/integration/my-first-api-test.js
+++ cypress/integration/my-first-api-test.js
@@ -4,8 +4,10 @@ it('Get users', function(){
    cy.request({
        method : 'GET',
        url : 'https://reqres.in/api/users/3'
-   }).then((response)=>{
+   })
+   .should((response)=>{
        console.log(response)
        expect(response.status).to.eq(200)
+       expect(response.body.data).has.property('id', 3)
    })
 })
